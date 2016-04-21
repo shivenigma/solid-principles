78e82cfc13982dc68f55d4c72a7ad4e45fa186dd
Vignesh M <vignesh.m@innoppl.com>
2016-04-21 14:16:56 +0530

    implemented liskov substitution principle violation refused bequest on adminUser class

--- adminUser.php
+++ adminUser.php
@@ -13,6 +13,15 @@ class adminUser extends User
 {
     private $admin;
 
+    public function calculateAge($dob)
+    {
+        return 0;
+        /*
+         * a override to the 
+         * Refused bequest: a class that overrides a method of a base class in such a way that the contract of the base class is not honored by the derived class
+         * */
+    }
+
     public function __construct($name, $email, $dob, $admin)
     {
         parent::__construct($name, $email, $dob);
--- userMapper.php
+++ userMapper.php
@@ -5,15 +5,16 @@ namespace Innoppl\Solid;
 
 class userMapper
 {
-    public function __construct()
+    public function connect()
     {
         //initialize databse connection
+        //used mysql
     }
-    public function saveUser()
+    public function save()
     {
         //some code to save in databse
     }
-    public function getUser()
+    public function get()
     {
         //some code to retrive user from the database
         //$result = from database
